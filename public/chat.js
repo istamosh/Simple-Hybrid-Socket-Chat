8c2179f528a1624ee5d23a6295f2109c4d12e000
istamosh <t0pramen19@gmail.com>
2020-11-18 23:36:09 +0700

    Update chat.js
    - added placeholder auto-scroll to bottom when someone sends a new message
    - added chat box scroll listener and STB (scroll to bottom) basic control

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,8 @@ var message = document.getElementById('message');
 userName = document.getElementById('userName'),
 btnSend = document.getElementById('send'),
 output = document.getElementById('output'),
-isTyping = document.getElementById('isTyping');
+isTyping = document.getElementById('isTyping'),
+chatBox = document.getElementById('chat-box');
 
 // emit events.
 // on send button click, callback a function.
@@ -27,6 +28,17 @@ message.addEventListener('keypress', () => {
   socket.emit('typing', userName.value);
 });
 
+var bottomSnapped = true;
+chatBox.addEventListener('scroll', () => {
+  var currentPos = chatBox.scrollTop + chatBox.offsetHeight;
+  if (currentPos == chatBox.scrollHeight) {
+    bottomSnapped = true;
+  }
+  else {
+    bottomSnapped = false;
+  }
+});
+
 // listen for events on server-side.
 // append pre-cooked code inside output's HTML as chat comp.
 // reset isTyping HTML to empty (this need more work).
@@ -34,6 +46,10 @@ socket.on('chat', function(data){
   isTyping.innerHTML = "";
   output.innerHTML += '<p><strong>' + data.userName +
   ': </strong>' + data.message + '</p>';
+
+  if (bottomSnapped) {
+    chatBox.scrollTo(0, chatBox.scrollHeight);
+  }
 });
 
 // listening on typing event with carried data from
